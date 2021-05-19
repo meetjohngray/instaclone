50a84618edb78edf26080c10d887b1e1d4cd2235
John Gray <john.edwin.gray@gmail.com>
2021-05-19 19:51:47 +1200

    comment out line

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
